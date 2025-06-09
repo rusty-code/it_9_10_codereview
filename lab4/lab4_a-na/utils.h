48d2db0b2a896ac99668ea973e981643b9564cdf
alena1805 <145384204+alena1805@users.noreply.github.com>
2025-06-09 10:50:18 +0500

    Update utils.h

--- lab4/lab4_a-na/utils.h
+++ lab4/lab4_a-na/utils.h
@@ -1,11 +1,10 @@
 #pragma once
 #ifndef UTILS_H
 #define UTILS_H
-
 #include <string>
 
-int input_int(const std::string& prompt, int min = INT_MIN, int max = INT_MAX);
-std::string input_string(const std::string& prompt, bool allow_empty = false);
-std::string input_name(const std::string& prompt);
+int input_int(const std::string& _prompt, int _min = INT_MIN, int _max = INT_MAX);
+std::string input_string(const std::string& _prompt, bool _allow_empty = false);
+std::string input_name(const std::string& _prompt);
 
 #endif // UTILS_H
